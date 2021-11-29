315de636f7cc5a1c2d5135c07e4909ec47407518
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-28 19:23:19 -0700

    Updated

--- CafeUtil.java
+++ CafeUtil.java
@@ -9,6 +9,28 @@ public class CafeUtil {
         return sum;
     }
 
+    public double getOrderTotal(double[] prices) {
+        double total = 0;
+        for( int i = 0; i < prices.length; i++){
+            total += prices[i];
+        }
+        return total;
+    }
+
+    public void displayMenu(ArrayList<String>menuItems){
+        String menu = "";
+        for( int i = 0; i < menuItems.length; i++){
+            System.out.println(i + menuItems[i]);
+        }
+    }
+
+    // public String printPriceChart(String product, double price, int maxQuantity) {
+    //     String item = product \n;
+    //     for( int i = 1; i < maxQuantity; i++) {
+    //         item += price \n
+    //     }
+    // }
+
 
 
 }
